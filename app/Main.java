94fc684bea6954d7864350eef55d1195cc1d6121
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-02-23 15:05:51 +0530

    Test functions in main

--- app/Main.java
+++ app/Main.java
@@ -1,1 +1,16 @@
+package app;
 
+import model.SavingsAccount;
+
+public class Main {
+    public static void main(String[] args){
+
+        SavingsAccount acc = new SavingsAccount("101", "Mohit", 10000);
+
+        acc.deposit(2000);
+        acc.withdraw(500);
+
+        System.out.println(acc.getBalance());
+        System.out.println(acc.getTransaction());
+    }
+}
