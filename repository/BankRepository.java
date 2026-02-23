c46c71c932caa7882d3daaddc02815f6aa0320d6
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-02-23 16:02:56 +0530

    Created BankRepository as mini storage with sigleton pattern and global instance access

--- repository/BankRepository.java
+++ repository/BankRepository.java
@@ -1,1 +1,38 @@
-.
+package repository;
+
+import model.Account;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankRepository{
+    
+    private static BankRepository instance;
+
+    private Map<String, Account> accounts;
+
+    private BankRepository(){
+        accounts = new HashMap<>();
+    }
+
+    public static BankRepository getInstance(){
+        if(instance == null){
+            instance = new BankRepository();
+        }
+        return instance;
+    }
+
+    public void addAccount(Account account) {
+        accounts.put(account.getAccountNumber(), account);
+    }
+
+    public Account getAccount(String accountNumber) {
+        return accounts.get(accountNumber);
+    }
+
+    public Map<String, Account> getAllAccounts(){
+        return accounts;
+    }
+
+
+}
