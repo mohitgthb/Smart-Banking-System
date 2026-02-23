b1e142f2506447f88e2499aa6fe3fd0968513163
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-02-23 14:46:05 +0530

    Extended current account class with overriden withdraw method

--- model/CurrentAccount.java
+++ model/CurrentAccount.java
@@ -1,1 +1,20 @@
-.
+package model;
+
+public class CurrentAccount extends Account {
+    
+    private double overdraftLimit = 5000;
+
+    public CurrentAccount(String accountNumber, String name, double balance){
+        super(accountNumber, name, balance);
+    }
+
+    @Override
+    public void withdraw(double amount){
+        if(balance + overdraftLimit >= amount) {
+            balance -= amount;
+            addTransaction(new Transaction("WITHDRAW", amount));
+        } else {
+            System.out.println("Overdraft limit exceeded");
+        }
+    }
+}
