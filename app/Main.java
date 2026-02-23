bfb003ca1763511aaafc56595f4dbc0a70b37446
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-02-23 17:17:18 +0530

    created AccountServices

--- app/Main.java
+++ app/Main.java
@@ -1,16 +1,25 @@
 package app;
 
 import model.SavingsAccount;
+import repository.BankRepository;
 
 public class Main {
     public static void main(String[] args){
 
+        BankRepository repo = BankRepository.getInstance();
         SavingsAccount acc = new SavingsAccount("101", "Mohit", 10000);
 
-        acc.deposit(2000);
-        acc.withdraw(500);
+        repo.addAccount(acc);
+
+        System.out.println(repo.getAccount("101").getName());
+
+        // acc.deposit(2000);
+        // acc.withdraw(500);
 
         System.out.println(acc.getBalance());
         System.out.println(acc.getTransaction());
+
+
+
     }
 }
--- service/AccountService.java
+++ service/AccountService.java
@@ -1,1 +1,49 @@
-.
+package service;
+
+import model.*;
+import repository.BankRepository;
+
+public class AccountService {
+    private BankRepository repo = BankRepository.getInstance();
+
+    public void createSavingsAccount(String accNo, String name, double balance){
+        Account acc = new SavingsAccount(accNo, name, balance);
+        repo.addAccount(acc);
+    }
+
+    public void createCurrentAccount(String accNo, String name, double balance){
+        Account acc = new CurrentAccount(accNo, name, balance);
+        repo.addAccount(acc);
+    }
+
+    public void deposit(String accNo, double amount){
+        Account acc = repo.getAccount(accNo);
+        if(acc != null){
+            acc.deposit(amount);
+        } else {
+            System.out.println("Account not found");
+        }
+    }
+
+    public void withdraw(String accNo, double amount){
+        Account acc = repo.getAccount(accNo);
+        if(acc != null){
+            acc.withdraw(amount);
+        } else {
+            System.out.println("Account not found");
+        }
+    }
+
+    public void transfer(String fromAcc, String toAcc, double amount){
+        Account sender = repo.getAccount(fromAcc);
+        Account receiver = repo.getAccount(toAcc);
+
+        if(sender == null || receiver == null ){
+            System.out.println("Invalid accounts");
+            return;
+        } else {
+            sender.withdraw(amount);
+            receiver.deposit(amount);
+        }
+    }
+}
